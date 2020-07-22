88506c01b9d9a2c2e61f461659029bad60f1f93a
Joe Grace <joeggrace@gmail.com>
2020-07-22 09:12:05 -0400

    new design made mobile friendly

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,3 @@
-const donut = document.querySelector(".donut--info");
-const donutImg = document.querySelector(".donut--img");
-const blog = document.querySelector(".blog--info");
-const blogImg = document.querySelector(".blog--img");
-const pet = document.querySelector(".pet--info");
-const petImg = document.querySelector(".pet--img");
 const aboutMe = document.querySelector(".about--me");
 const navAboutMe = document.querySelector(".nav--about--me");
 const skills = document.querySelector(".skills");
@@ -79,42 +73,6 @@ observerResume = new IntersectionObserver((entries) => {
     { threshold: 1 }
   );
 });
-
-donutImg.onmouseover = function () {
-  donut.style.display = "block";
-};
-
-donut.onmouseover = function () {
-  donut.style.display = "block";
-};
-
-donut.onmouseout = function () {
-  donut.style.display = "none";
-};
-
-blogImg.onmouseover = function () {
-  blog.style.display = "block";
-};
-
-blog.onmouseover = function () {
-  blog.style.display = "block";
-};
-
-blog.onmouseout = function () {
-  blog.style.display = "none";
-};
-
-petImg.onmouseover = function () {
-  pet.style.display = "block";
-};
-
-pet.onmouseover = function () {
-  pet.style.display = "block";
-};
-
-pet.onmouseout = function () {
-  pet.style.display = "none";
-};
 
 observerMe.observe(aboutMe);
 observerSkills.observe(skills);
